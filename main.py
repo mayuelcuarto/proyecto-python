3ec35b55a6386eb347eb6d4d5fbb21b6e4e1551e
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-02-22 05:07:44 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Proyecto Python y Mysql:
+- Abrir asistente
+- Login o registro
+- Si elegimos registro, nos creará un usuario en la bd
+- Si elegimos login, identifica al usuario y nos preguntará
+- Crear nota, mostrar notas, borrarlas.
+"""
