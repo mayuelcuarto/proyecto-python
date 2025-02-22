5240c204c9c8bc0194993cd500a3570c2e77ec3e
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-02-22 05:19:07 -0500

    Prueba de cambio en local

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ Proyecto Python y Mysql:
 - Si elegimos registro, nos creará un usuario en la bd
 - Si elegimos login, identifica al usuario y nos preguntará
 - Crear nota, mostrar notas, borrarlas.
+
+Prueba de cambios
 """
