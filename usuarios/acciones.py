3fa03103b69ccbe25579785723b02c0ba04ec169
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-02-26 02:04:01 -0500

    Implementacion de menu de acciones

--- usuarios/acciones.py
+++ usuarios/acciones.py
@@ -35,4 +35,25 @@ class Acciones:
             print(f"Login incorrecto, inténtalo más tarde")
 
     def proximasAcciones(self, usuario):
-        return None
+        print("""
+            Acciones disponibles:
+            - Crear nota (crear)
+            - Mostrar tus notas (mostrar)
+            - Eliminar nota (eliminar)
+            - Salir (salir)
+        """)
+
+        accion = input("¿Qué quieres hacer?: ")
+
+        if accion == "crear":
+            print("Vamos a crear")
+            self.proximasAcciones(usuario)
+        elif accion == "mostrar":
+            print("Vamos a mostrar")
+            self.proximasAcciones(usuario)
+        elif accion == "eliminar":
+            print("Vamos a eliminar")
+            self.proximasAcciones(usuario)
+        elif accion == "salir":
+            print(f"Ok {usuario[1]}, hasta pronto!!")
+            exit()
